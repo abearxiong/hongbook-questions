0edd434c3b67afdcebabfba61fb042587438854c
abearxiong <xiongxiao1012@outlook.com>
2025-02-20 01:01:50 +0800

    fix: fix question base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+const isDev = process.env.NODE_ENV === 'development';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -10,6 +11,7 @@ export default defineConfig({
   define: {
     DEV_SERVER: JSON.stringify(process.env.NODE_ENV === 'development'),
   },
+  base: isDev ? '/' : '/apps/questions/',
   esbuild: {
     target: 'esnext',
   },
